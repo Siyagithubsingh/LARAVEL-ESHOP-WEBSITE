9e13ed59f811f60dd47614c8b8b3795737a6dec2
siya <ssiyasingh2002@gmail.com>
2024-11-10 14:13:56 +0530

    first commit

---
+++ resources/views/frontend/pages/about-us.blade.php
@@ -0,0 +1,107 @@
+@extends('frontend.layouts.master')
+
+@section('title','Eshop || About Us')
+
+@section('main-content')
+
+	<!-- Breadcrumbs -->
+	<div class="breadcrumbs">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="bread-inner">
+						<ul class="bread-list">
+							<li><a href="index1.html">Home<i class="ti-arrow-right"></i></a></li>
+							<li class="active"><a href="blog-single.html">About Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Breadcrumbs -->
+
+	<!-- About Us -->
+	<section class="about-us section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-12">
+						<div class="about-content">
+							@php
+								$settings=DB::table('settings')->get();
+							@endphp
+							<h3>Welcome To <span>Eshop</span></h3>
+							<p>Welcome to Eshop! We are passionate about making online shopping easy and enjoyable for everyone. Our mission is to provide a seamless shopping experience by offering a wide selection of quality products at competitive prices.
+
+At Eshop, we believe in customer satisfaction above all else. Our dedicated team is here to assist you every step of the way, ensuring you find exactly what you need. We are constantly updating our inventory to bring you the latest trends and best deals.
+
+Thank you for choosing Eshop as your go-to shopping destination. We look forward to serving you!
+
+</p>
+							<div class="button">
+								<a href="{{route('blog')}}" class="btn">Our Blog</a>
+								<a href="{{route('contact')}}" class="btn primary">Contact Us</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-12">
+						<div class="about-img overlay">
+							{{-- <div class="button">
+								<a href="https://www.youtube.com/watch?v=nh2aYrGMrIE" class="video video-popup mfp-iframe"><i class="fa fa-play"></i></a>
+							</div> --}}
+							<img src="@foreach($settings as $data) {{$data->photo}} @endforeach" alt="@foreach($settings as $data) {{$data->photo}} @endforeach">
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- End About Us -->
+
+
+	<!-- Start Shop Services Area -->
+	<section class="shop-services section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-rocket"></i>
+						<h4>Free shiping</h4>
+						<p>Orders over Rs 1000</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-reload"></i>
+						<h4>Free Return</h4>
+						<p>Within 30 days returns</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-lock"></i>
+						<h4>Sucure Payment</h4>
+						<p>100% secure payment</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-tag"></i>
+						<h4>Best Peice</h4>
+						<p>Guaranteed price</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Shop Services Area -->
+
+	@include('frontend.layouts.newsletter')
+@endsection
